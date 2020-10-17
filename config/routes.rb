f5916804290724a0c95ead120504958e37126551
4429-wd <w4429d0gken@gmail.com>
2020-10-17 20:41:08 +0900

    trace-chatspace

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,15 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'stocks#index'
-  get 'stocks', to: 'stocks#index'
-  get 'stocks/new', to:'stocks#new'
-  post 'stocks', to: 'stocks#create' 
+  resources :stocks, only: [:index, :new, :create]
+  resources :users, only: [:index, :edit, :update]
+  resources :groups, only: [:new, :create, :edit, :update] do
+    resources :messages, only: [:index, :create]
+    namespace :api do
+      resources :messages, only: :index, defaults: { format: 'json'}
+  # get 'stocks', to: 'stocks#index'
+  # get 'stocks/new', to:'stocks#new'
+  # post 'stocks', to: 'stocks#create'
+    end
+  end 
 end
