a9c8a6412dec468073dcff0c4210ebd39c1e7288
4429-wd <w4429d0gken@gmail.com>
2020-10-20 22:08:25 +0900

    fix-stockpage

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root 'stocks#index'
-  resources :stocks, only: [:index, :show, :new]
+  resources :stocks, only: [:index, :new, :update :destroy]
   resources :users, only: [:index, :edit, :update]
   resources :groups, only: [:new, :create, :edit, :update] do  
     resources :messages, only: [:index, :create]
